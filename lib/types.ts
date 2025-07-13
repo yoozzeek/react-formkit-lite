e9a72fd711e9c5bef88d6e4d673c93af5853de14
25x8 <kocherginji@gmail.com>
2025-07-13 11:39:49 +0700

    ADD: Main types

---
+++ lib/types.ts
@@ -0,0 +1,71 @@
+import type {
+  SelectFormikHandler,
+  SelectSimpleHandler,
+  SelectOptionType,
+} from "@/components/Select";
+import type { MouseEvent, FocusEvent } from "react";
+
+export type Size = "xxs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+
+export type Position = "center" | "right" | "left" | "top" | "bottom";
+
+export type Variant =
+  | "light"
+  | "dark"
+  | "success"
+  | "danger"
+  | "warning"
+  | "none"
+  | "primary"
+  | "info"
+  | "black";
+
+export interface CommonFieldProps {
+  id: string;
+  name?: string;
+  label?: string | null;
+  helpText?: string | null;
+  placeholder?: string | null;
+  fullWidth?: boolean;
+  disabled?: boolean;
+  required?: boolean;
+  resetDisabled?: boolean;
+  error?: string | boolean | string[];
+  onFocus?: (e: MouseEvent | FocusEvent) => void;
+  onReset?: (field: string, value: string | number, shouldValidate?: boolean | undefined) => void;
+}
+
+export interface CustomSelectFieldProps<T> {
+  id: string;
+  name: string;
+  label?: string | null;
+  disabled?: boolean;
+  placeholder?: string | null;
+  error?: string | string[] | boolean;
+  required?: boolean;
+  helpText?: string | null;
+  popupLabel?: string | null;
+  dpPosition?: Position;
+  dpFullscreen?: boolean;
+  dpWidth?: number;
+  fixedOptions?: SelectOptionType<T>[];
+  onFocus?: (e?: MouseEvent | FocusEvent) => void;
+  onChange: SelectSimpleHandler<T> | SelectFormikHandler;
+}
+
+export interface CustomSingleSelectFieldProps<T> extends CustomSelectFieldProps<T> {
+  value: string | number;
+}
+
+export interface CustomMultipleSelectFieldProps<T> extends CustomSelectFieldProps<T> {
+  min?: number;
+  max?: number;
+  values: string[] | number[];
+}
+
+export type UploadedFileItem = {
+  url: string;
+  name: string;
+  type: string; // "image" | "pdf";
+  isPlaceholder?: boolean;
+};
