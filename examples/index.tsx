f6f1c9d925c61db85c425303794a78744b3fc4db
Andrew <zeek@tuta.com>
2025-07-22 10:06:02 +0700

    ADD: Examples for Button and Badge components (#4)

---
+++ examples/index.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BadgeExamplesPage from "./BadgeExamples";
+import ButtonExamplesPage from "./ButtonExamples";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <header>
+      <h1>react-formkit-lite demo</h1>
+      <a href="https://github.com/yoozzeek/react-formkit-lite" target="_blank">
+        Open on Github
+      </a>
+      <p>
+        A lightweight collection of React components for building forms. Works with your own form
+        state or integrates smoothly with Formik.
+      </p>
+    </header>
+
+    <div>
+      <h2>Components</h2>
+      <nav
+        style={{
+          marginTop: "1em",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 21,
+        }}
+      >
+        <a href="#">Badge</a>
+        <a href="#">Button</a>
+        <a href="#">Text</a>
+        <a href="#">Select</a>
+        <a href="#">Date</a>
+        <a href="#">Range</a>
+        <a href="#">Checkbox</a>
+        <a href="#">Radio</a>
+        <a href="#">Switch</a>
+        <a href="#">MultipleFiles</a>
+        <a href="#">UploadArea</a>
+      </nav>
+    </div>
+
+    <div
+      style={{
+        marginTop: "1em",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1em",
+      }}
+    >
+      <BadgeExamplesPage />
+      <ButtonExamplesPage />
+    </div>
+
+    <footer>
+      <p>Feel free to add more examples and contribute.</p>
+      <div>
+        <strong>Authors:</strong>
+        <ul>
+          <li>
+            <a href="https://bitkind.org/about" target="_blank">
+              Bitkind.org
+            </a>{" "}
+            [inbox@bitkind.org]
+          </li>
+          <li>
+            <a href="https://github.com/yoozzeek" target="_blank">
+              Andrew
+            </a>{" "}
+            [zeek@tuta.com]
+          </li>
+        </ul>
+      </div>
+      <strong>License: MIT</strong>
+    </footer>
+  </React.StrictMode>,
+);
