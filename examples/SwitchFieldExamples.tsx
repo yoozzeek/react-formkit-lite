1f6ab3a392feddbd7ff5782fd3701325bc02fd06
Andrew <zeek@tuta.com>
2025-07-29 20:24:53 +0700

    ADD: Switch field examples

---
+++ examples/SwitchFieldExamples.tsx
@@ -0,0 +1,43 @@
+import { type CSSProperties, useState } from "react";
+import SwitchField from "@/components/Switch";
+
+const blockStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "1em",
+};
+
+export default function SwitchFieldExamples() {
+  const [value, setValue] = useState(false);
+  const [value2, setValue2] = useState(false);
+  return (
+    <section id="switch_field_examples">
+      <h2>Switch field examples</h2>
+      <div>
+        <h3>Default</h3>
+        <SwitchField
+          id="switch_field_1"
+          name="switch_field_1"
+          label="Label"
+          value={value}
+          onChange={setValue}
+        />
+      </div>
+      <div>
+        <h3>Help text</h3>
+        <SwitchField
+          id="switch_field_2"
+          name="switch_field_2"
+          label="Label"
+          helpText="Help text"
+          value={value2}
+          onChange={setValue2}
+        />
+      </div>
+      <div>
+        <h3>Flags</h3>
+        <div style={blockStyle}>Soon...</div>
+      </div>
+    </section>
+  );
+}
